d110cccb580f5c9204c2a11aa643b800db01a539
Eunseok OH <lspk.lpg@gmail.com>
2022-02-15 06:53:28 +0900

    styledComponent library practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div className="App">
-      <SassComponent />
+      <StyledComponent/>
     </div>
   );
 }
