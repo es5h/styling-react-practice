86a22ca7179c5a3f3d26690b4e070721f6b34c55
Eunseok OH <lspk.lpg@gmail.com>
2022-02-15 07:06:16 +0900

    styledComponent media practice

--- src/StyledComponent.js
+++ src/StyledComponent.js
@@ -1,9 +1,19 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 const Box = styled.div`
   background: ${props => props.color || 'blue'};
   padding: 1rem;
   display: flex;
+
+  width: 1024px;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const Button = styled.button`
@@ -13,34 +23,34 @@ const Button = styled.button`
   padding: 0.5rem;
   display: flex;
   align-items: center;
-  box-sizing: border-box;;
+  box-sizing: border-box;
   font-size: 1rem;
   font-weight: 600;
 
   &:hover {
     background: rgba(255, 255, 255, 0.9);
   }
-  
-  ${props => props.inverted && css`
-  background: none;
-  border: 2px solid white;
-  color: white;
 
-  &:hover {
-    background: white;
-    color: black;
-  }
-`};
-  
-  &+button {
+  ${props => props.inverted && css`
+    background: none;
+    border: 2px solid white;
+    color: white;
+
+    &:hover {
+      background: white;
+      color: black;
+    }
+  `};
+
+  & + button {
     margin-left: 1rem;
   }
 `;
 
 const StyledComponent = () => (
-  <Box color = "black">
+  <Box color="black">
     <Button> hi </Button>
-    <Button inverted = {true}> only border </Button>
+    <Button inverted={true}> only border </Button>
   </Box>
 );
 
